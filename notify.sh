388b42a3f5a26bff1d6462a09f075bc9318a1408
kdingn <koukou3335@gmail.com>
2021-12-06 10:52:12 +0900

    #1 対応

--- notify.sh
+++ notify.sh
@@ -8,7 +8,8 @@ span=300
 
 # notifyer
 notify(){
-  url=<URL>
+  url=https://discord.com/api/webhooks/916524788762157076/nwYp9qKZvuvQZlB253STJ0USKOQrO9-rFO3ZRilxoZ3qyfRZQvNXZ_8BD9OZb1Xhp_4a
+  #url=https://discord.com/api/webhooks/915626907914108939/YOzhShq33Iw9oUCYeOJUqfVb9LjI_wE2m2Gi6ubFvX-6MLN5EnLsuetwAkunfN8eTN1U
   curl -H "Content-Type: application/json" -X POST -d '{"username": "minecraftBot", "content":"'"$1"'"}' "$url"
 }
 
@@ -37,7 +38,7 @@ while true; do
     var_l=$(seconds ${line:1:8})
     var_s=$(($var_t - $var_l))
     if [ $var_s -le 0 ] ; then
-        var_s=$((var_s + 24*3600))
+      var_s=$((var_s + 24*3600))
     fi
     # 一定時間内のログに絞り込み
     if [ $var_s -le $span ] ; then
@@ -47,8 +48,13 @@ while true; do
   done
   echo "$LINES"
 
-  # notify
-  notify "$LINES"
+  # ファイル更新が span 以内の場合に通知を行う
+  var_f0=$(date +%s)
+  var_f=$(date -r $path +%s)
+  if [ $((var_f0-var_f)) -le $span ] ; then
+    # notify
+    notify "$LINES"
+  fi
 
   # 一定時間待機
   sleep $span
